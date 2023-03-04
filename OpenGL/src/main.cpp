4317717f22c8cef1de8bd9d31ec0a92834073813
Franco Caredda <franco.caredda12345@gmail.com>
2023-03-05 00:41:49 +0100

    Description of the vertex array problem in Old OpenGL

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -2,61 +2,37 @@
 
 #include <iostream>
 
-float vertices[] = {
-        -0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
+float verteciesRect[] = {
+	-0.50f,  -0.50f,
+	-0.50f,  -0.25f,
+	-0.25f,  -0.50f,
+	-0.25f,  -0.25f
+};
 
-        -0.5f, -0.5f,  0.5f,
-         0.5f, -0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
+uint32_t rectIndecies[] = {
+	0, 1, 2, 2, 1, 3
+};
 
-        -0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-
-         0.5f,  0.5f,  0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
-
-        -0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f,  0.5f,
-         0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f, -0.5f,
-
-        -0.5f,  0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f,  0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f
+float verteciesTriangle[] = {
+	0.25f, -0.50f,
+	0.25f, -0.25f,
+	0.50f, -0.50f
 };
 
 void Draw()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-    glEnableClientState(GL_VERTEX_ARRAY);
 
-    // Every time we need to reset data in GL_VERTEX_ARRAY buffer
-    
-    glVertexPointer(3, GL_FLOAT, 0, vertices);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+	glEnableClientState(GL_VERTEX_ARRAY);
 
-    // After executing glDrawArrays current data in GL_VERTEX_ARRAY buffer is not valid
+	// We need to write first object from CPU to GPU directly every frame.
+	// In OpenGL 1.1 exists only one vertex buffer
+	glVertexPointer(2, GL_FLOAT, 0, verteciesRect);
+	glDrawElements(GL_TRIANGLES, sizeof(rectIndecies) / sizeof(uint32_t), GL_UNSIGNED_INT, rectIndecies);
+
+	// We need to write second object from CPU to GPU directly every frame.
+	glVertexPointer(2, GL_FLOAT, 0, verteciesTriangle);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	glutSwapBuffers();
 }
