ff150b1f4b994da42d36a1365a818e80bf1cd35a
Franco Caredda <franco.caredda12345@gmail.com>
2023-03-04 09:01:00 +0100

    Object coloring

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -1,4 +1,5 @@
 #include <freeglut.h>
+#include <cstring>
 
 #include <iostream>
 #include <stb_image.h>
@@ -6,11 +7,41 @@
 uint8_t* img_data = nullptr;
 int width, height, channels;
 
+const char* title = "Smile";
+
+float vertecies[] = {
+	// VERTEX_POS	VERTEX_COLOR
+	-1.0f, -1.0f,	1.0f, 0.0f, 0.0f,
+	-1.0f,  0.0f,	0.0f, 1.0f, 0.0f,
+	 0.0f, -1.0f,	0.0f, 0.0f, 1.0f,
+	 0.0f,  0.0f,   1.0f, 1.0f, 0.0f
+};
+
+uint32_t indecies[] = {
+	0, 1, 2, 2, 1, 3
+};
+
 void Draw()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	glRasterPos2d(0.2, 0.2);
 	glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, img_data);
+
+
+	int length = strlen(title);
+
+	glRasterPos2d(0.40, -0.1);
+	for (int i = 0; i < length; i++)
+		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, title[i]);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+
+	glVertexPointer(2, GL_FLOAT, 5 * sizeof(float), vertecies);
+	glColorPointer(3, GL_FLOAT, 5 * sizeof(float), &vertecies[2]);
+
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indecies);
 
 	glutSwapBuffers();
 }
@@ -33,7 +64,6 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowPosition(450, 450);
 	glutInitWindowSize(600, 480);
-
 
 	int windowHandle = glutCreateWindow("Hello world!");
 
